bd0d89da50a72abd3b9380a2b9bae39d8f2cec1c
sevimozsoy <sevimozsoy30@gmail.com>
2022-04-26 14:36:47 +0300

    Navbar finished but it's static.

--- src/Components/navbar/Navbar.jsx
+++ src/Components/navbar/Navbar.jsx
@@ -1,10 +1,27 @@
 import React from 'react';
 import './navbar.scss';
+import SearchIcon from '@mui/icons-material/Search';
+import NotificationsIcon from '@mui/icons-material/Notifications';
+import adminPhoto from '../../assets/images/admin.png'
 
 const Navbar = () => {
   return (
     <div className="navbar">
-      <div className="wrapper">Navbar</div>
+      <div className="wrapper">
+        <div className="search">
+          <input type="text" placeholder='Search..' />
+          <SearchIcon />
+        </div>
+        <div className="items">
+          <div className="item">
+          <NotificationsIcon/>
+          <div className="counter">1</div>
+          </div>
+          <div className="item">
+          <img src={adminPhoto} alt="" className='avatar'/>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
